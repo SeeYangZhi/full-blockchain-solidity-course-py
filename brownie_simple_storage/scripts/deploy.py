88cf5dc8f166d248e671fa333308fe8b978f3454
See Yang Zhi <yangzhi.see@gmail.com>
2021-09-30 23:18:43 +0800

    Lesson 5: Brownie Simple Storage
    - Brownie Console

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,14 +1,23 @@
-from brownie import accounts, config
+from brownie import accounts, config, SimpleStorage, network
 import os
 
 
 def deploy_simple_storage():
-    # account = accounts[0]
-    # account = accounts.load("rinkeby-testnet")
-    # account = accounts.add(os.getenv("PRIVATE_KEY"))
-    account = accounts.add(config["wallets"]["from_key"])
+    account = get_account()
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.favoriteNumber()
+    print(stored_value)
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    updated_stored_value = simple_storage.favoriteNumber()
+    print(updated_stored_value)
 
-    print(account)
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 
 def main():

655459423af943d8cee4e52ca7d406263cdbd119
See Yang Zhi <yangzhi.see@gmail.com>
2021-10-03 19:00:27 +0800

    Lesson 6: Brownie Fund Me
    - Set up
    - Contract verification through etherscan api
    - Flattening
    - Deploying to local chains
    - Mocked price feeds from chainlink-mix repo

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,10 +1,24 @@
-from brownie import FundMe
-from scripts.tools import get_account
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.tools import get_account, deploy_mocks, LOCAL_BLOCKCHAIN_ENVIRONMENT
+from web3 import Web3
 
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from": account})
+    # Pass the price feed adddress to our fundme contract
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENT:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
     print(f"Contract deployed to {fund_me.address}")
 
 
